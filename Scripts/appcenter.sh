fde066e0724c781f16f82e5ce734b09626f756b3
Szymon Kazmierczak <szymonkaz@gmail.com>
2020-07-29 14:25:58 +0100

    test appcenter config

--- Scripts/appcenter.sh
+++ Scripts/appcenter.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+appcenter apps list --token $APP_CENTER_TOKEN
 RELEASE_NOTES="$(git log -1 --pretty=short)"
 appcenter distribute release --token $APP_CENTER_TOKEN --app "skazmierczak-zendesk.com/apps/UnitySDK-QA-1" --group "Collaborators" --file $FILE_PATH --release-notes "$RELEASE_NOTES" --silent
