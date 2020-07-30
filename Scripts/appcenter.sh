f125ec1464f8037ac334e7dcde5717bb988047cf
Szymon Kazmierczak <Simon-Kaz@users.noreply.github.com>
2020-07-30 09:51:26 +0100

    Update appcenter.sh

--- Scripts/appcenter.sh
+++ Scripts/appcenter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 RELEASE_NOTES="$(git log -1 --pretty=short)"
-appcenter distribute release --token $APP_CENTER_TOKEN --app "skazmierczak-zendesk.com/apps/UnitySDK-QA-1" --group "Collaborators" --file $FILE_PATH --release-notes "$RELEASE_NOTES" --silent
+appcenter distribute release --token $APP_CENTER_TOKEN --app "skazmierczak-zendesk.com/UnitySDK-QA-1" --group "Collaborators" --file $FILE_PATH --release-notes "$RELEASE_NOTES" --silent
