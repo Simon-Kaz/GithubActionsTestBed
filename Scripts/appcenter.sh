49e1459341fe94832d071b3d49fbce6d9317925d
Szymon Kazmierczak <szymonkaz@gmail.com>
2020-07-30 12:27:04 +0100

    gfasfas

--- Scripts/appcenter.sh
+++ Scripts/appcenter.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-RELEASE_NOTES="$(git log -1 --pretty=short)"
-appcenter distribute release --token $APP_CENTER_TOKEN --app $APP_NAME --group "Collaborators" --file $FILE_PATH --release-notes "$RELEASE_NOTES" --silent
+APP_NAME=@1
+DISTRIBUTION_GROUP=@2
+FILE_PATH=@3
+
+
+RELEASE_NOTES="[Build for PR: $PR_NUMBER: $PR_TITLE \nBranch: $PR_BRANCH, commit: $GITHUB_SHA, \nurl: $PR_URL]"
+echo RELEASE_NOTES
+# appcenter distribute release --token $APP_CENTER_TOKEN --app $APP_NAME --group $DISTRIBUTION_GROUP --file $FILE_PATH --release-notes $RELEASE_NOTES --silent
+
+
+
